cbc81c9cf4a0e207fdcc3fec1ef612c4bdff58d4
Nimishayadav1031 <nimiyadav777@gmail.com>
2025-11-14 13:07:16 +0530

    Add files via upload

---
+++ src/recommendations/recommendation_engine.py
@@ -0,0 +1,33 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from chroma_db import KnowledgeBaseDB
+from recommendations.query_processor import QueryProcessor
+
+class RecommendationEngine:
+    def __init__(self):
+        self.kb = KnowledgeBaseDB()
+        self.processor = QueryProcessor()
+
+    def get_recommendations(self, customer_query: str, top_k: int = 3):
+        processed = self.processor.process_query(customer_query)
+
+        results = self.kb.search(processed['cleaned'], n_results=top_k)
+
+        recommendations = []
+
+        if results['ids'] and results['ids'][0]:
+            for i in range(len(results['ids'][0])):
+                recommendations.append({
+                    'article_id': results['ids'][0][i],
+                    'source': results['metadatas'][0][i]['source_file'],
+                    'content': results['documents'][0][i],
+                    'relevance': 1.0 - (i * 0.1)
+                })
+
+        return {
+            'query_info': processed,
+            'recommendations': recommendations,
+            'total_found': len(recommendations)
+        }
